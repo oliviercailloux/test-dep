d39ba10e27bfe3c743679906e63074e384e30ebf
Olivier Cailloux <olivier.cailloux@gmail.com>
2017-08-17 10:04:31 +0200

    Init: bug artifact

---
+++ src/main/java/io/github/oliviercailloux/test_dep/App.java
@@ -0,0 +1,12 @@
+package io.github.oliviercailloux.test_dep;
+
+public class App {
+
+	public static void main(String[] args) {
+		new App().proceed();
+	}
+
+	public void proceed() {
+		System.out.println("Hello World!");
+	}
+}
